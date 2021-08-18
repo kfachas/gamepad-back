ef2d175c2c7cdfc48fb2f132f21e7cf318964c7a
Kevin <kfachas@gmail.com>
2021-08-18 12:17:19 +0200

    add reviews rating sort

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,10 +7,22 @@ const Reviews = require("../models/Reviews");
 
 router.post("/game/reviews", async (req, res) => {
   try {
-    const reviews = await Reviews.find({ game: req.fields.gameId }).populate(
-      "owner"
-    );
-    res.status(200).json(reviews);
+    const reviews = await Reviews.find({ game: req.fields.gameId }).populate({
+      path: "owner",
+      select: "account",
+    });
+
+    const result = [];
+    for (let i = 0; i < reviews.length; i++) {
+      for (let j = 0; j < reviews.length; j++) {
+        if (reviews[i].rate.result > reviews[j].rate.result) {
+          result.unshift(reviews[i]);
+          i++;
+        }
+      }
+    }
+
+    res.status(200).json(result);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -19,31 +31,50 @@ router.post("/game/reviews", async (req, res) => {
 router.post("/game/reviewRating", isAuthenticated, async (req, res) => {
   try {
     const review = await Reviews.findById(req.fields.id);
+    for (let i = 0; i < review.rate.users.length; i++) {
+      if (review.rate.users[i] === req.user) {
+        return res
+          .status(400)
+          .json({ message: "Vous ne pouvez voté qu'une fois" });
+      }
+    }
 
-    if (req.fields.rate === 1) {
-      review.rate += req.fields.rate;
-    } else {
-      review.rate -= req.fields.rate;
+    if (req.fields.rate) {
+      review.rate.result += req.fields.rate;
+      review.rate.users.push(req.user);
+      review.markModified("rate.rating");
     }
 
     await review.save();
     res.status(200).json(review.rate);
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).json(error.message);
   }
 });
 
 router.post("/game/addReview", isAuthenticated, async (req, res) => {
   try {
-    const newReview = new Reviews({
-      owner: req.user,
-      title: req.fields.title,
-      rate: 0,
-      text: req.fields.text,
+    const searchReview = await Reviews.findOne({
       game: req.fields.game,
+      owner: req.user,
     });
-
-    await newReview.save();
+    if (!searchReview) {
+      const newReview = new Reviews({
+        owner: req.user,
+        title: req.fields.title,
+        rate: {
+          result: 0,
+          rating: [{ id: req.user, value: 0 }],
+        },
+        text: req.fields.text,
+        game: req.fields.game,
+      });
+      await newReview.save();
+    } else {
+      return res
+        .status(401)
+        .json({ message: "U already add a review for this game" });
+    }
     res.status(200).json(req.user);
   } catch (error) {
     res.status(400).json({ message: error.message });
